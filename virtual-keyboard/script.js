6c2004e79dbb94c1fbe9c77f97ab3802af298a0a
Artem-Kalugin <Aharlo4@gmail.com>
2020-10-29 18:47:24 +0300

    feat: add shift and language change

--- virtual-keyboard/script.js
+++ virtual-keyboard/script.js
@@ -4,7 +4,57 @@ const Keyboard = {
     keysContainer: null,
     keys: []
   },
-
+  shiftSwitcher: {
+    "1" : "!",
+    "2" : "@",
+    "3" : "#",
+    "4" : "$",
+    "5" : "%",
+    "6" : "^",
+    "7" : "&",
+    "8" : "*",
+    "9" : "(",
+    "0" : ")",
+    "," : "<",
+    "?" : "/",
+    "." : ">",
+    "!" : "1",
+    "@" : "2",
+    "#" : "3",
+    "$" : "4",
+    "%" : "5",
+    "^" : "6",
+    "&" : "7",
+    "*" : "8",
+    "(" : "9",
+    ")" : "0",
+    "<" : ",",
+    ">" : ".",
+    "/" : "?"
+  },
+  shiftSwitcherRu: {
+    "1" : "!",
+    "2" : "\"",
+    "3" : "№",
+    "4" : ";",
+    "5" : "%",
+    "6" : ":",
+    "7" : "?",
+    "8" : "*",
+    "9" : "(",
+    "0" : ")",
+    "!" : "1",
+    "\"" : "2",
+    "№" : "3",
+    ";" : "4",
+    "%" : "5",
+    ":" : "6",
+    "?" : "7",
+    "*" : "8",
+    "(" : "9",
+    ")" : "0",
+    "." : ",",
+  },
   eventHandlers: {
     oninput: null,
     onclose: null
@@ -12,7 +62,11 @@ const Keyboard = {
 
   properties: {
     value: "",
-    capsLock: false
+    cursorPos: "",
+    capsLock: false,
+    shift: false,
+    langRu : false
+
   },
 
   init() {
@@ -26,7 +80,7 @@ const Keyboard = {
     this.elements.keysContainer.appendChild(this._createKeys());
 
     this.elements.keys = this.elements.keysContainer.querySelectorAll(".keyboard__key");
-
+     
     // Add to DOM
     this.elements.main.appendChild(this.elements.keysContainer);
     document.body.appendChild(this.elements.main);
@@ -36,21 +90,44 @@ const Keyboard = {
       element.addEventListener("focus", () => {
         this.open(element.value, currentValue => {
           element.value = currentValue;
+          element.focus();
         });
       });
     });
   },
-
+  _toggleChangeLang(){
+    this.properties.langRu = !this.properties.langRu;
+    this.properties.capslock = false;
+    this.properties.shift = false;
+    
+    var cleaner = document.querySelector(".keyboard__keys"); 
+    cleaner.remove();
+    var parent = document.querySelector(".keyboard");
+    this.elements.keysContainer = document.createElement("div");
+    this.elements.keysContainer.classList.add("keyboard__keys");
+    this.elements.keysContainer.appendChild(this._createKeys());
+    this.elements.keys = this.elements.keysContainer.querySelectorAll(".keyboard__key");
+    parent.appendChild(this.elements.keysContainer);
+  },
   _createKeys() {
-    const fragment = document.createDocumentFragment();
-    const keyLayout = [
+    const fragment = document.createDocumentFragment(); 
+    const KL = [
       "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
-      "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+      "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "EN",
       "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
-      "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
-      "space"
+      "Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+      "nav-bef", "space" , "nav-aft"
     ];
-
+    const KLRU = [
+      "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+      "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з","х", "ъ", "RU",
+      "caps", "ф", "ы", "в", "а", "п", "р", "о", "л", "д","ж","э","enter",
+      "Shift", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".",
+      "nav-bef", "space" , "nav-aft"
+    ];
+    let keyLayout ="";
+    if (this.properties.langRu) keyLayout = KLRU;
+    else keyLayout = KL;
     // Creates HTML for an icon
     const createIconHTML = (icon_name) => {
       return `<i class="material-icons">${icon_name}</i>`;
@@ -58,8 +135,8 @@ const Keyboard = {
 
     keyLayout.forEach(key => {
       const keyElement = document.createElement("button");
-      const insertLineBreak = ["backspace", "p", "enter", "?"].indexOf(key) !== -1;
-
+      const insertLineBreak = ["backspace", "enter", "?", "RU", "EN"].indexOf(key) !== -1 || (this.properties.langRu && ["."].indexOf(key) !==-1);
+      
       // Add attributes/classes
       keyElement.setAttribute("type", "button");
       keyElement.classList.add("keyboard__key");
@@ -82,7 +159,7 @@ const Keyboard = {
 
           keyElement.addEventListener("click", () => {
             this._toggleCapsLock();
-            keyElement.classList.toggle("keyboard__key--active", this.properties.capsLock);
+            keyElement.classList.toggle("keyboard__key--active");
           });
 
           break;
@@ -90,13 +167,41 @@ const Keyboard = {
         case "enter":
           keyElement.classList.add("keyboard__key--wide");
           keyElement.innerHTML = createIconHTML("keyboard_return");
-
           keyElement.addEventListener("click", () => {
             this.properties.value += "\n";
             this._triggerEvent("oninput");
           });
-
           break;
+        case "RU":
+            keyElement.classList.add("keyboard__key");
+            keyElement.textContent = key;
+            keyElement.addEventListener("click", () => {
+              this._toggleChangeLang();
+            });
+            break;
+        case "EN":
+              keyElement.classList.add("keyboard__key");
+              keyElement.textContent = key;
+              keyElement.addEventListener("click", () => {
+                this._toggleChangeLang();
+              });
+              break;
+        case "nav-bef":
+            keyElement.classList.add("keyboard__key--wide");
+            keyElement.innerHTML = createIconHTML("navigate_before");
+            keyElement.addEventListener("click", () => {
+              this.properties.cursorPos--;
+              this._triggerEvent("oninput");
+            });
+            break;  
+        case "nav-aft":
+              keyElement.classList.add("keyboard__key--wide");
+              keyElement.innerHTML = createIconHTML("navigate_next");
+              keyElement.addEventListener("click", () => {
+                this.properties.cursorPos++;
+                this._triggerEvent("oninput");
+              });
+              break;  
 
         case "space":
           keyElement.classList.add("keyboard__key--extra-wide");
@@ -109,13 +214,13 @@ const Keyboard = {
 
           break;
 
-        case "done":
-          keyElement.classList.add("keyboard__key--wide", "keyboard__key--dark");
-          keyElement.innerHTML = createIconHTML("check_circle");
-
+        case "Shift":
+          keyElement.classList.add("keyboard__key--advwide", "keyboard__key--activatable");
+          keyElement.textContent = "Shift";
           keyElement.addEventListener("click", () => {
-            this.close();
-            this._triggerEvent("onclose");
+            this._toggleCapsLock();
+            this._toggleShift();
+            keyElement.classList.toggle("keyboard__key--active");
           });
 
           break;
@@ -124,7 +229,7 @@ const Keyboard = {
           keyElement.textContent = key.toLowerCase();
 
           keyElement.addEventListener("click", () => {
-            this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+            this.properties.value += keyElement.innerText;
             this._triggerEvent("oninput");
           });
 
@@ -151,8 +256,22 @@ const Keyboard = {
     this.properties.capsLock = !this.properties.capsLock;
 
     for (const key of this.elements.keys) {
-      if (key.childElementCount === 0) {
+      if (key.textContent.length == 1) {
         key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+      }
+    }
+  },
+  _toggleShift() {
+    this.properties.shift = !this.properties.shift;
+    let decider;
+    if (this.properties.langRu) decider = this.shiftSwitcherRu;
+    else decider=this.shiftSwitcher;
+    for (const key of this.elements.keys) {
+      if (isFinite(key.textContent) 
+       || ([",", ".", "?"].indexOf(key.textContent) != -1)
+       || isFinite(decider[key.textContent])
+       || ([",", ".", "?"].indexOf(decider[key.textContent]) != -1 )) {
+        key.textContent = decider[key.textContent];
       }
     }
   },
